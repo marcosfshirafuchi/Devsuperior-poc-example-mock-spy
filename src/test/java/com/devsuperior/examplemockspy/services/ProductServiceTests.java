40c2b6870f584931fc1c0c1cf3f5676c9526e3af
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2026-06-24 21:43:21 -0300

    Update com id existente

--- src/test/java/com/devsuperior/examplemockspy/services/ProductServiceTests.java
+++ src/test/java/com/devsuperior/examplemockspy/services/ProductServiceTests.java
@@ -80,4 +80,38 @@ public class ProductServiceTests {
             ProductDTO result = serviceSpy.insert(productDTO);
         });
     }
+
+    @Test
+    public void updateShouldReturnProductDTOWhenIdExistsAndValidData(){
+        ProductService serviceSpy = Mockito.spy(service);
+        Mockito.doNothing().when(serviceSpy).validateData(productDTO);
+
+        ProductDTO result = serviceSpy.update(existingId, productDTO);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(result.getId(),existingId);
+
+    }
+
+    @Test
+    public void updateShouldReturnInvalidDataExceptionWhenIdExistsAndProductNameIsBlank(){
+        productDTO.setName("");
+        ProductService serviceSpy = Mockito.spy(service);
+        Mockito.doThrow(InvalidDataException.class).when(serviceSpy).validateData(productDTO);
+
+        Assertions.assertThrows(InvalidDataException.class,()->{
+            ProductDTO result = serviceSpy.update(existingId,productDTO);
+        });
+    }
+
+    @Test
+    public void updateShouldReturnInvalidDataExceptionWhenIdExistsAndProductPriceIsNegativeOrZero(){
+        productDTO.setPrice(-5.0);
+        ProductService serviceSpy = Mockito.spy(service);
+        Mockito.doThrow(InvalidDataException.class).when(serviceSpy).validateData(productDTO);
+
+        Assertions.assertThrows(InvalidDataException.class,()->{
+            ProductDTO result = serviceSpy.update(existingId,productDTO);
+        });
+    }
 }
